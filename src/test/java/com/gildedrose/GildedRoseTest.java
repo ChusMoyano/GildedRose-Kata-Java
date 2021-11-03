2a56ce275301c461fe837e5c9e592ac6e74978ed
Jesus Moyano <jesus.moyano@JesusMoyanos-MacBook-Pro.local>
2021-11-03 16:43:30 +0100

    Aged and Negative Quality test

--- src/test/java/com/gildedrose/GildedRoseTest.java
+++ src/test/java/com/gildedrose/GildedRoseTest.java
@@ -19,4 +19,31 @@ class GildedRoseTest {
         assertEquals(40, app.items[0].quality);
     }
 
+
+   @Test
+   void agedBrieTest() {
+       Item[] items = new Item[] { new Item("Aged Brie", 5, 0) };
+       GildedRose gildedRose = new GildedRose(items);
+       gildedRose.updateQuality();
+
+       assertEquals("Aged Brie", gildedRose.items[0].name);
+       assertEquals(4, gildedRose.items[0].sellIn);
+       assertEquals(1, gildedRose.items[0].quality);
+   }
+
+    @Test
+    void negativeQuality() {
+       Item[] items = new Item[] { new Item("Macbook", 10, 2) };
+       GildedRose gildedRose = new GildedRose(items);
+
+       //After 5 days
+       for(int i = 0; i < 5; i++) {
+           gildedRose.updateQuality();
+       }
+
+       assertEquals("Macbook", gildedRose.items[0].name);
+       assertEquals(5, gildedRose.items[0].sellIn);
+       assertEquals(0, gildedRose.items[0].quality);
+   }
+
 }
